494131647315a01a7171593dd36201a401e67d25
Orlando <tortonorlando.omg@gmail.com>
2021-09-26 23:29:31 -0500

    Empty list UI fix 2

--- src/components/todo-list/index.tsx
+++ src/components/todo-list/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { List } from "antd";
 import "./index.styles.css";
-import { Todo } from "../../shared/types";
 import { useAppSelector } from "../../hooks";
-import todosSlice from "../../redux/todosSlice";
 
 interface TodoListProps {
   children: React.ReactNode;
@@ -11,11 +9,17 @@ interface TodoListProps {
 
 const TodoList = ({ children }: TodoListProps) => {
   const isLoading = useAppSelector((state) => state.todos.isLoading);
+  const todos = useAppSelector((state) => state.todos.todos);
 
   return (
     <List
       bordered
       size="large"
+      style={{
+        flexDirection: "column",
+        justifyContent: "center",
+        display: todos.length ? "initial" : "flex",
+      }}
       loading={isLoading}
       className="todo-list-container"
     >
